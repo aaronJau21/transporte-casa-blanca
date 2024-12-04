a1843d94e6758f50418f7237d0f620f197381903
Aaron <aaronjau21@gmail.com>
2024-12-04 17:01:11 -0500

    correption card

--- src/about/StandOut.tsx
+++ src/about/StandOut.tsx
@@ -12,9 +12,9 @@ export const StandOut = () => {
               className="mb-4 h-12 w-12 text-pink-500"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
               <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z"></path>
             </svg>
@@ -47,8 +47,8 @@ export const StandOut = () => {
                   className="h-4 w-4"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
                   ></path>
                 </svg>

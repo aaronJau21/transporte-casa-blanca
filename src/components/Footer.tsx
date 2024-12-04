66d622a0b0f392aa004af679c21913b819ab848f
Aaron <aaronjau21@gmail.com>
2024-12-04 00:15:30 -0500

    responsive en footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,31 +4,33 @@ export const Footer = () => {
   return (
     <footer className="bg-primary">
       <div className="">
-        <div className="h-80 flex justify-between items-center container mx-auto w-11/12">
-          <Image
-            src="/logo.jpg"
-            alt="logo Casa Blanca Transporte"
-            width={170}
-            height={170}
-          />
+        <div className="sm:h-80 sm:flex sm:justify-between sm:items-center md:container md:mx-auto md:w-11/12">
+          <div className="flex justify-center">
+            <Image
+              src="/logo.jpg"
+              alt="logo Casa Blanca Transporte"
+              width={170}
+              height={170}
+            />
+          </div>
 
-          <div className="text-white">
+          <div className="text-white text-center sm:text-start">
             <h4 className="text-3xl font-bold mb-4">Contacto</h4>
             <p>Direccion : Terminal terrestre Plaza norte</p>
             <p>numero: +51 933 524 495</p>
             <p>Email: zabaletaerinson@gmail.com</p>
             <p>lunes-sabado : 9:00 AM - 5:00 PM</p>
           </div>
-          <div className="text-white">
+          <div className="text-white text-center sm:text-start">
             <h4 className="text-3xl font-bold mb-4">Horario</h4>
             <p>Traslados y conexiones aeroportuarias</p>
             <p>Traslados de personal y ejecutivos</p>
             <p>Vehículos a disposición con chofer</p>
-            <p>translado rutas largas</p>
+            <p>Translado rutas largas</p>
           </div>
         </div>
         <div className="h-24 border-t border-white">
-          <div className="flex h-full justify-between items-center container mx-auto w-11/12 text-white">
+          <div className="flex flex-col justify-center h-full sm:justify-between items-center container mx-auto w-11/12 text-white">
             <p>© 2024 Transporte Casa Blanca</p>
             <p>Powered by Transporte Casa Blanca</p>
           </div>

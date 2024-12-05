63327771f9a4b37617e1e2bf5913555b5adefcfc
Aaron <aaronjau21@gmail.com>
2024-12-05 04:41:51 -0500

    change dark

--- src/home/Vide.tsx
+++ src/home/Vide.tsx
@@ -1,7 +1,9 @@
 export const Vide = () => {
   return (
-    <section className="max-w-2xl px-6 py-8 mx-auto bg-white dark:bg-gray-900">
-      <h3 className="text-4xl text-center font-black">Algunos de nuestros cLientes</h3>
+    <section className="max-w-2xl px-6 py-8 mx-auto bg-white ">
+      <h3 className="text-4xl text-center font-black">
+        Algunos de nuestros cLientes
+      </h3>
       <main className="mt-8">
         <h4 className="mt-6 text-gray-700 dark:text-gray-200 text-2xl font-bold">
           Maritsa Fabian{" "}

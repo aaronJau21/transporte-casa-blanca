8be1091b6d260b7292c7922d01ac404245b89011
Aaron <aaronjau21@gmail.com>
2024-12-08 13:03:16 -0500

    add video 2

--- src/home/Vide.tsx
+++ src/home/Vide.tsx
@@ -1,28 +1,35 @@
-export const Vide = () => {
-  return (
-    <section className="max-w-2xl px-6 py-8 mx-auto bg-white ">
-      <h3 className="text-4xl text-center font-black">
-        Experiencias de nuestros Clientes
-      </h3>
-      <div className="mt-8">
-        <h4 className="mt-6 text-gray-900 text-2xl font-bold">
-          Maritsa Fabian{" "}
-        </h4>
+import { VideoItem } from "./VideoItem";
 
-        <p className="mt-2 leading-loose text-gray-900 text-justify">
-          Recomiendo ampliamente a la empresa por su excelente servicio. Su
+const itemsVideos = [
+  {
+    path: "/images/home/video1.mp4",
+    nombre: "Maritsa Fabian",
+    comentario: `
+    Recomiendo ampliamente a la empresa por su excelente servicio. Su
           responsabilidad, puntualidad y atención son excepcionales, realmente
           agradecemos la rapidez y eficiencia para todos mis envíos a
           Chanchamayo - Perú
-        </p>
+    `,
+  },
+  {
+    path: "/images/home/video2.mp4",
+    nombre: "Monolo Perez",
+    comentario: `
+   RECOMEND
+    `,
+  },
+];
 
-        <div className="flex justify-center mt-5">
-          <video
-            className="w-96 h-96"
-            controls
-            src="/images/home/video1.mp4"
-          ></video>
-        </div>
+export const Vide = () => {
+  return (
+    <section className=" px-6 py-8 mx-auto bg-white ">
+      <h3 className="text-4xl text-center font-black">
+        Experiencias de nuestros Clientes
+      </h3>
+      <div className="mt-8 lg:grid lg:grid-cols-3 container mx-auto w-11/12">
+        {itemsVideos.map((item) => (
+          <VideoItem key={item.nombre} {...item} />
+        ))}
       </div>
     </section>
   );
